c3c8009caec35ca8f18a66f6982181fb8a6f27ec
tanvirtbf <tanver018765@gmail.com>
2024-05-26 12:01:20 +0600

    start with file submit ui

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,21 +1,25 @@
 const Button = ({ whichForm, setWhichForm }) => {
   return (
-    <div className="flex justify-end gap-3">
-      {whichForm >= 2 && (
-        <button
-          className="px-8 py-2 hover:bg-[#554141] rounded bg-black font-semibold text-sm text-white transition-background duration-500 ease-in-out"
-          onClick={() => setWhichForm((prevState) => prevState - 1)}
-        >
-          Previous
-        </button>
+    <>
+      {whichForm < 3 && (
+        <div className="flex justify-end gap-3">
+          {whichForm >= 2 && (
+            <button
+              className="px-8 py-2 hover:bg-[#554141] rounded bg-black font-semibold text-sm text-white transition-background duration-500 ease-in-out"
+              onClick={() => setWhichForm((prevState) => prevState - 1)}
+            >
+              Previous
+            </button>
+          )}
+          <button
+            className="px-8 py-2 bg-[#FF216D] rounded hover:bg-pink-700 font-semibold text-sm text-white transition-background duration-500 ease-in-out"
+            onClick={() => setWhichForm((prevState) => prevState + 1)}
+          >
+            Next
+          </button>
+        </div>
       )}
-      <button
-        className="px-8 py-2 bg-[#FF216D] rounded hover:bg-pink-700 font-semibold text-sm text-white transition-background duration-500 ease-in-out"
-        onClick={() => setWhichForm((prevState) => prevState + 1)}
-      >
-        Next
-      </button>
-    </div>
+    </>
   );
 };
 
